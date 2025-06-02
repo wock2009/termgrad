0b690f180e5338118754a3f5bccaeff750b3be13
wock2009 <idki76004@gmail.com>
2025-06-02 21:21:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Package name > Gardient
+# Made by > wock2009
+# Site > https://coderhangout.xyz
+
+# Why choose gradient? > Gardient package is a clean and colourful package.
+For your terminal tools. Its easy to apply onto your CLI tools.
+Gardient package got many cool colors.
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gardient",
+    version="0.1.0",
+    author="Wock 2009",
+    description="A python package for nice clean colors for your terminal tools!",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://coderhangout.xyz",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
+    keywords="terminal colors gradient text cli",
+    license="MIT",
+    project_urls={
+        "Homepage": "https://coderhangout.xyz",
+    },
+)
